6a1e9b5e7ebefd5cdbf60dea36932c4128bb6cab
swethavarnaa <swethavarnaa@gmail.com>
2021-01-25 21:59:51 +0530

    Updates

---
+++ precache-manifest.0a2f2765204d968ea83c25a7e4ed4bf3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20faa2866eb7f14ab7f9bd6408f6b13c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8ffa17bbd163b3bb762",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "5f2f23a7e6facbe8d932",
+    "url": "/static/js/2.99c8101f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.99c8101f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ffa17bbd163b3bb762",
+    "url": "/static/js/main.ee6a6216.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "bfe2346b796b40b76365606c569fe5b8",
+    "url": "/static/media/swethavarnaa_u.bfe2346b.pdf"
+  },
+  {
+    "revision": "5ce59136d66f91705849f7382cbd83e5",
+    "url": "/static/media/varnaa.5ce59136.jpeg"
+  }
+]);
